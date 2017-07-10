a15ddf2ad9d04b22b49b7f5b739a2abdae2d2ce8
arlen <burton.arlen.1990@gmail.com>
2017-07-10 14:05:45 -0700

    setup java for bottom menu on user activity

--- app/src/main/java/com/inboundrx/paulsensbeaconsapp/ui/ui/UserActivity.java
+++ app/src/main/java/com/inboundrx/paulsensbeaconsapp/ui/ui/UserActivity.java
@@ -11,12 +11,40 @@ import com.inboundrx.paulsensbeaconsapp.R;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.aboutButton) ImageView mAboutButton;
+    @Bind(R.id.homeButton) ImageView mHomeButton;
+    @Bind(R.id.rewardsButton) ImageView mRewardsButton;
+    @Bind(R.id.historyButton) ImageView mHistoryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        ButterKnife.bind(this);
+        mAboutButton.setOnClickListener(this);
+        mHomeButton.setOnClickListener(this);
+        mRewardsButton.setOnClickListener(this);
+        mHistoryButton.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v){
+        if (v == mAboutButton){
+            Intent intent = new Intent(UserActivity.this, AboutActivity.class);
+            startActivity(intent);
+        }
+        if (v == mRewardsButton){
+            Intent intent = new Intent(UserActivity.this, RewardsActivity.class);
+            startActivity(intent);
+        }
+        if (v == mHistoryButton){
+            Intent intent = new Intent(UserActivity.this, HistoryActivity.class);
+            startActivity(intent);
+        }
+        if (v == mHomeButton){
+            Intent intent = new Intent(UserActivity.this, LandingActivity.class);
+            startActivity(intent);
+        }
     }
 }
