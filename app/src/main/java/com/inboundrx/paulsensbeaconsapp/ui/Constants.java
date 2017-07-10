68a4fd39f9828b9c43827b7bed035161eb0d0b38
arlen <burton.arlen.1990@gmail.com>
2017-07-10 14:39:14 -0700

    setup about bage Java

--- app/src/main/java/com/inboundrx/paulsensbeaconsapp/ui/Constants.java
+++ app/src/main/java/com/inboundrx/paulsensbeaconsapp/ui/Constants.java
@@ -5,4 +5,9 @@ package com.inboundrx.paulsensbeaconsapp.ui;
  */
 
 public class Constants {
+    public  static  final String PAULSENS_BIO = "Five ways we are here for you, the Patient \\n     If you are thinking of switching to Paulsen’s, you are making the right choice. We are a locally-owned and operated historical pharmacy. Our primary mission is to help our customers achieve optimal health.  Here are five things we do differently than your normal drugstore. \\n   Medication Synchronization:  If you take multiple medications we will manage the process to ensure that all your medication refills on the same date each month. \\n    Multiple Package Offerings: Have your medications packaged in vials, single dose blister packs, multi-dose blister packs, or pouch packaging. \\n     Multiple Pick-Up Options: Come in to pick up your medications each month, receive them in the mail or by free home delivery.\\n    Appointment Based Model:  Come in or call and we’ll set up a customized appointment program for you.\\n   Medication Therapy Management (MTM): medical care provided by pharmacists whose aim is to optimize drug therapy and improve therapeutic outcomes for patients.";
+    public static final String PAULSENS_HOURS = "Monday - Friday 9:00am - 6:30pm\\nSaturday 10:00am - 2:00pm";
+    public static final String PAULSENS_WEBSITE = "https://www.paulsenspharmacy.com";
+    public static final String PAULSENS_ADDRESS = "4246 NE Sandy Blvd\\nPortland, OR 97213";
+    public static final String PAULSENS_PHONE = "(503)287-1163";
 }
